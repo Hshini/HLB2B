06d9fd8ef53e9976af63dcb43e3aee3c0691c815
Shini <halimshini@hotmail.com>
2023-04-29 23:27:47 -0400

    added styles to navbar

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav, Button,} from 'react-bootstrap';
 import '../assets/css/style.css'
 
 function NavBar() {
   return (
     <Navbar bg="primary" expand="lg">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" style={{margin:'10px'}}>
-        <Nav style={{display:'flex',justifyContent:'end'}}>
-          <Link to='/aboutus'><Button variant="primary">About</Button>{' '}</Link>
-          <Link to='/mission'><Button variant="primary">Our Mission</Button>{' '}</Link>
-          <Link to='/organization'><Button variant="primary">Our Organization</Button>{' '}</Link>
-          <Link to='/service'><Button variant="primary">Our Service</Button>{' '}</Link>
-          <Link to='/contact'><Button variant="primary">Contact</Button>{' '}</Link>
+      <Navbar.Collapse id="basic-navbar-nav" >
+        <Nav >
+          <div  style={{ display: 'flex', justifyContent: 'end', alignItems: 'end', alignContent: 'end' }}>
+            <Link to='/aboutus'><Button variant="primary">About</Button>{' '}</Link>
+            <Link to='/mission'><Button variant="primary">Our Mission</Button>{' '}</Link>
+            <Link to='/organization'><Button variant="primary">Our Organization</Button>{' '}</Link>
+            <Link to='/service'><Button variant="primary">Our Service</Button>{' '}</Link>
+            <Link to='/contact'><Button variant="primary">Contact</Button>{' '}</Link>
+          </div>
         </Nav>
-        </Navbar.Collapse>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
